52f7a0badde94eabdb06b2f3db90f3c476adf717
Ben Jenkins <benjenkinsv95@gmail.com>
2024-12-22 15:05:37 -0600

    Smooth behavior for nav links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-scree pb-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Contact />
-        <section>About</section>
-        <section>Projects</section>
-        <section>Skills</section>
+        <section id='about'>About</section>
+        <section id='projects'>Projects</section>
+        <section id='skills'>Skills</section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         Copyright yada
